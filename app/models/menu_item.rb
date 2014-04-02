673fc0f49206306f47445ecbb6dc426d62c5bd5f
Derrick Wolbert <wolbert.derrick@gmail.com>
2014-04-01 20:01:48 -0400

    trying to add storage s3 to model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -6,8 +6,19 @@ class MenuItem < ActiveRecord::Base
   validates :price, presence: true,
             numericality: true,
             format: { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }
-  has_attached_file :photo
+
+
+    has_attached_file :photo #only thing truly needed. below is for production env.
+    :storage        => :s3                                                 ,
+                      :s3_credentials => {:bucket            => ENV['wolberine'],
+                                          :access_key_id     => ENV['AKIAJZZ6NPB63BRFJPGQ'],
+                                          :secret_access_key => ENV['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']},
+                      :s3_protocol    => "https"                                             ,
+                      :s3_host_name   => "s3.amazonaws.com"
+  
   validates_attachment_presence :photo
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   #
- end
+end
+
+
